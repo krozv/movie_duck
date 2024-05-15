d300c13a5d3d649adc34a3ab9548d92a8ac19ffb
krozv <krozv.code@gmail.com>
2024-05-15 15:44:07 +0900

    feat: 대댓글 기능 추가

--- backend/movie/views.py
+++ backend/movie/views.py
@@ -1,8 +1,10 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
-from .serializers import MovieListSerializer, MovieSerializer, CommentSerializer
-from .models import Movie, Comment
+from .serializers import MovieListSerializer, MovieSerializer
+from .serializers import CommentSerializer, ReplySerializer
+from .serializers import CommentReplySerializer
+from .models import Movie, Comment, Reply
 from django.shortcuts import get_object_or_404
 
 @api_view(['GET'])
@@ -15,13 +17,10 @@ def movie_main(request):
 def movie_detail(request, movie_pk):
   # 단일 게시글 (댓글 포함) 조회
   if request.method == 'GET':
-    try: 
-      movie = get_object_or_404(Movie, pk=movie_pk)
-      serializer = MovieSerializer(movie)
-      return Response(serializer.data, status=status.HTTP_200_OK)
-    except:
-       return Response({'error':'존재하지 않는 영화'}, status=status.HTTP_404_NOT_FOUND)
-
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    serializer = MovieSerializer(movie)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+   
 @api_view(['POST'])
 def create_comment(request, movie_pk):
   movie = get_object_or_404(Movie, pk=movie_pk)
@@ -34,8 +33,14 @@ def create_comment(request, movie_pk):
   serializer = CommentSerializer(comment)
   return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['PUT', 'DELETE', 'POST'])
+@api_view(['GET', 'PUT', 'DELETE', 'POST'])
 def comment(request, movie_pk, comment_pk):
+  # 댓글 조회(대댓글 포함)
+  if request.method == 'GET':
+    comment = get_object_or_404(Comment, pk=comment_pk)
+    serializer = CommentReplySerializer(comment)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+  
   # 댓글 수정
   if request.method == 'PUT':
     comment = get_object_or_404(Comment, pk=comment_pk)
@@ -48,4 +53,32 @@ def comment(request, movie_pk, comment_pk):
   if request.method == 'DELETE':
     comment = get_object_or_404(Comment, pk=comment_pk)
     comment.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+  
+  # 대댓글 생성
+  if request.method == 'POST':
+    comment = get_object_or_404(Comment, pk=comment_pk)
+    content = request.data.get('content')
+    reply = Reply.objects.create(
+      comment=comment,
+      author=request.user,
+      content=content
+    )
+    serializer = ReplySerializer(reply)
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['PUT', 'DELETE'])
+def reply(request, movie_pk, comment_pk, reply_pk):
+  # 대댓글 수정
+  if request.method == 'PUT':
+    reply = get_object_or_404(Reply, pk=reply_pk)
+    serializer = ReplySerializer(reply, data=request.data, partial=True)
+    if serializer.is_valid(raise_exception=True):
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+  
+  # 대댓글 삭제
+  if request.method == 'DELETE':
+    reply = get_object_or_404(Reply, pk=reply_pk)
+    reply.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)

bec53fcde2de205b3b088d12896b945ff2e1d05e
krozv <krozv.code@gmail.com>
2024-05-15 15:46:25 +0900

    build: entry app 삭제

--- backend/entry/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.entry_main, name='entry-main'),
-    path('<int:entry_pk>/', views.entry_detail, name='entry-detail'),
-]

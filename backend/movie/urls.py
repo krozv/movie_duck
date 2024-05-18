8e1062abab124009f317c4eae14ce9e972761b9b
krozv <krozv.code@gmail.com>
2024-05-19 02:23:07 +0900

    feat: API 문서 수정

--- backend/movie/urls.py
+++ backend/movie/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('<int:movie_pk>/comment/', views.create_comment),
     path('<int:movie_pk>/comment/<int:comment_pk>/', views.comment),
     path('<int:movie_pk>/comment/<int:comment_pk>/<int:reply_pk>/', views.reply),
+    path('boxoffice/', views.box_office),
 ]
